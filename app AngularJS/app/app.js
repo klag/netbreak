456beab3dc6b392f5070002607cd3d7c2c9e3195
nicscap <scapin.nicolo@gmail.com>
2017-05-01 23:24:08 +0200

    rimossa la mia roba di angular

--- app AngularJS/app/app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('my-App', ['jsonService']);
-
-app.controller('ApiHome', function($scope, JsonService) {
-    JsonService.get(function(home){
-        $scope.LatestApi = home.LatestApi;
-    });
-});
