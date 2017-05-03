7fcbafb040a12546dee2549bad23ea246e3f9475
nicscap <scapin.nicolo@gmail.com>
2017-05-03 10:59:34 +0200

    pushata di backup

--- APIM/app/services/api_home.js
+++ APIM/app/services/api_home.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myapp', []);
 app.controller('api_homeCtrl', function($scope, $http) {
     $http.get("http://localhost:8100/homepage_ms_list").then(function(response) {
         $scope.mslistdata = response.data;
